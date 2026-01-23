efd5935eaaff19c13d3666f4b8ef0bdc2aaaf4d6
Ahmedm2002 <ahmedmujtaba0159@gmail.com>
2026-01-23 14:55:52 +0500

    configured database & added validations packages

---
+++ src/configs/db.js
@@ -0,0 +1,12 @@
+import { Pool } from "pg";
+
+const pool = new Pool({
+  host: process.env.DATABASE_HOST,
+  port: process.env.DATABASE_PORT,
+  database: process.env.DATABASE_NAME,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  max: 10,
+});
+
+export default pool;
